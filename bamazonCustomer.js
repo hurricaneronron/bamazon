a4ab0b50701b9c265b86b3d4d4303fee87dd1f59
hurricaneronron <vprzybyl@gmail.com>
2018-04-11 22:29:33 -0700

    additional question prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 var inq = require('inquirer')
-var pmpt = inq.createPromptModule( )
+var prompt = inq.createPromptModule( )
 
 var mysql = require('mysql')
 var config = mysql.createConnection({
@@ -22,11 +22,20 @@ function close ( ) {
   })
 }
 
+var begin = [
+  {
+    type: 'list',
+    name: 'openShop',
+    message: 'Press Enter to view shop inventory',
+    choices: ['View Inventory', "I'm ready to purchase"]
+  }
+]
+
 var buy = [
   {
     type: 'input',
     name: 'whichId',
-    message: 'What is the id of the product you would like to purchase?',
+    message: 'What is the item_id of the product you would like to purchase?',
   },
   {
     type: 'input',
@@ -35,26 +44,52 @@ var buy = [
   }
 ]
 
-open ( )
-config.query('SELECT * FROM products', function (e, r) {
-  if (e) throw e
-  for( var i = 0; i < r.length; i++ ){
-    for(var key in r[i]) {
-      console.log(key + ' : ' + r[i][key])
-    }
-  }
-})
-close ( )
+function start ( ) {
+  prompt(begin).then(function (r) {
+    switch (r.openShop) {
+      case 'View Inventory' :
+        showInventory ( )
+        break
+      case "I'm ready to purchase" :
+        purchase ( )
+        break
+      }
+  })
+}
 
-// function start ( ) {
-//   prompt(q1).then(function (r) {
-//     switch (r.x) {
-//       case 'Create an item' :
-//         nextQs( )
-//         break
-//       case 'View all items' : 
-//         returnFunction( )
-//         break
-//     }
+function showInventory ( ) {
+  open ( )
+  config.query('SELECT * FROM products', function (e, r) {
+    if (e) throw e
+    for( var i = 0; i < r.length; i++ ){
+      for(var key in r[i]) {
+        console.log(key + ': ' + r[i][key])
+      }
+    }
+  })
+  close ( )
+}
+
+function purchase ( ) {
+  prompt(buy).then(function (r) {
+    console.log(r)
+  })
+}
+
+// function delData(nm, bd) {
+//   open( )
+//   var qry = `
+//   DELETE FROM products
+//   SET ?
+//   `
+//   var values = {
+//     name: nm,
+//     currentBid: bd
+//   }
+//   config.query(qry, values, function (e, r) {
+//     if (e) throw e
 //   })
+//   close( )
 // }
+
+start ( )
