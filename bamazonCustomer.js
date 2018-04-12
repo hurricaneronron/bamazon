1a805443f259997226f7a977f7d0e0215b5dec21
hurricaneronron <vprzybyl@gmail.com>
2018-04-11 21:51:48 -0700

    display items function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
-var inquire = require('inquirer')
-var prompt = inquire.createPromptModule( )
+var inq = require('inquirer')
+var pmpt = inq.createPromptModule( )
 
 var mysql = require('mysql')
 var config = mysql.createConnection({
@@ -10,15 +10,51 @@ var config = mysql.createConnection({
   socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 })
 
-config.connect(function (e) {
-  if (e) throw e
-})
+function open ( ) {
+  config.connect(function (e) {
+    if (e) throw e
+  })
+}
 
+function close ( ) {
+  config.end(function (e) {
+    if (e) throw e
+  })
+}
+
+var buy = [
+  {
+    type: 'input',
+    name: 'whichId',
+    message: 'What is the id of the product you would like to purchase?',
+  },
+  {
+    type: 'input',
+    name: 'quantity',
+    message: 'How many of the item would you like to purchase?'
+  }
+]
+
+open ( )
 config.query('SELECT * FROM products', function (e, r) {
   if (e) throw e
-  console.log(r)
+  for( var i = 0; i < r.length; i++ ){
+    for(var key in r[i]) {
+      console.log(key + ' : ' + r[i][key])
+    }
+  }
 })
+close ( )
 
-config.end(function (e) {
-  if (e) throw e
-})
+// function start ( ) {
+//   prompt(q1).then(function (r) {
+//     switch (r.x) {
+//       case 'Create an item' :
+//         nextQs( )
+//         break
+//       case 'View all items' : 
+//         returnFunction( )
+//         break
+//     }
+//   })
+// }
